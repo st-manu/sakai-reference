233c37b7297ea6d721f652b16b9345d97b727dfc
manu •• <mfuster99@outlook.es>
2026-04-20 15:17:14 +0200

    sam review

--- docs/conversion/sakai_25_1-25_2_mysql_conversion.sql
+++ docs/conversion/sakai_25_1-25_2_mysql_conversion.sql
@@ -1,3 +1,4 @@
 -- SAK-51938
 alter table lesson_builder_pages add column hiddenFromNavigation bit(1) default 0;
+update lesson_builder_pages set hiddenFromNavigation = 0;
 -- END SAK-51938
--- docs/conversion/sakai_25_1-25_2_oracle_conversion.sql
+++ docs/conversion/sakai_25_1-25_2_oracle_conversion.sql
@@ -1,3 +1,4 @@
 -- SAK-51938
 alter table lesson_builder_pages add hiddenFromNavigation number(1,0) default 0;
+update lesson_builder_pages set hiddenFromNavigation = 0;
 -- END SAK-51938
